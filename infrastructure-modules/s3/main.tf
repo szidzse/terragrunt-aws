e7682fa29c1736e8f187118666ffa2919b89d221
Sós Gellért <gellertsos@gmail.com>
2025-09-29 21:38:46 +0200

    create server side encryption for s3

--- infrastructure-modules/s3/main.tf
+++ infrastructure-modules/s3/main.tf
@@ -24,3 +24,28 @@ resource "aws_s3_object" "test_upload_bucket" {
     Environment = "Dev"
   }
 }
+
+# STEP 3: Enable the server side encryption using KMS key
+resource "aws_kms_key" "s3_bucket_kms_key" {
+  description             = "KMS key for S3 bucket"
+  deletion_window_in_days = 7
+  tags = {
+    Name = "KMS key for S3 bucket"
+  }
+}
+
+resource "aws_kms_alias" "s3_bucket_kms_key_alias" {
+  name          = "alias/s3_bucket_kms_key_alias"
+  target_key_id = aws_kms_key.s3_bucket_kms_key.key_id
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_encryption_with_kms_key" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_bucket_kms_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
